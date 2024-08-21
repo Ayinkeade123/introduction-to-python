b44ea422a71809be7775efdb77001e020d3ba5db
Ayinkeade123 <jejelolayinke@gmail.com>
2024-08-21 13:19:18 +0100

    creating a variable number

---
+++ 2_data_type.py
@@ -0,0 +1,16 @@
+num = 10
+num_type = type(num) # Check the data type of num and store it in a variable num_type
+print(num_type) # Print the variable 
+print("num_type") # Print the string num_type
+
+name_1 = "Zainab"
+name_2 = "Blessing"
+name_3 = "Muideen"
+name_4 = "Richard"
+name_5 = "Mr Richard"
+
+data_type_name = type(name_4)
+print(data_type_name)
+
+sola_age = float(40)
+print(sola_age)
