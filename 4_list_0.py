e890e3d96eb98a4783d13e71ba3ecaaf8663587e
Ayinkeade123 <jejelolayinke@gmail.com>
2024-08-23 11:27:59 +0100

    4_list_0.py

--- 4_list_0.py
+++ 4_list_0.py
@@ -1,6 +1,30 @@
 myTechList= ["python", "c++", "java", "mysql"] 
-# appending HTML to myTechList
+myTechList2= ["AWS"]
+            
+ #appending HTML to myTechList
 myTechList.append("HTML")
-print (myTechList)
+
+# print item on index 2
+print(myTechList[2]) 
+
+#print the length of your list
+print (len (myTechList))
+
+#insert AWS , cybersecurity
+myTechList.insert (5,"cybersecurity")
+
+#extend myTechList to myTechList2
+myTechList.extend (myTechList2)
+
+#remove cybersecurity
+myTechList.remove ("cybersecurity")
+
+#pop AWD
+myTechList.pop (5)
+
+#del HTML
+del myTechList [2]
 
 print (myTechList)
+
+
