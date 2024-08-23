0fe8d677add5985659ae008741761c33f17e006f
Ayinkeade123 <jejelolayinke@gmail.com>
2024-08-23 10:34:28 +0100

    myTechList

--- 4_list_0.py
+++ 4_list_0.py
@@ -1,2 +1,6 @@
-myTechList= ["python", "c++", "java", "mysql"]
+myTechList= ["python", "c++", "java", "mysql"] 
+# appending HTML to myTechList
+myTechList.append("HTML")
+print (myTechList)
+
 print (myTechList)
