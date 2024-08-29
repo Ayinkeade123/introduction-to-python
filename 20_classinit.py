7e9a27acb7c2dc2e9dd2f7370dc444143bf1d627
Ayinkeade123 <jejelolayinke@gmail.com>
2024-08-29 14:51:38 +0100

    20_classinit.py

---
+++ 20_classinit.py
@@ -0,0 +1,13 @@
+class MyName:
+    def __init__(self, name, age, sex, color):
+        self.name = name
+        self.age = age
+        self.sex = sex
+        self.color = color
+
+p1 = MyName("zainab", 70, "f", "black")     
+
+print(p1.name) 
+print(p1.age) 
+print(p1.sex)
+print(p1.color)
