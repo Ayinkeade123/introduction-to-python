3c66e319e914cef008883bda3b7775f52450acd5
Ayinkeade123 <jejelolayinke@gmail.com>
2024-08-31 16:14:12 +0100

    4_list_0.py

--- 4_list_0.py
+++ 4_list_0.py
@@ -1,11 +1,15 @@
 myTechList= ["python", "c++", "java", "mysql"] 
 myTechList2= ["AWS"]
-            
+for x in myTechList:
+    print(x[2])
+
+
+
  #appending HTML to myTechList
 myTechList.append("HTML")
 
 # print item on index 2
-print(myTechList[2]) 
+print(myTechList[-2]) 
 
 #print the length of your list
 print (len (myTechList))
@@ -19,10 +23,10 @@ myTechList.extend (myTechList2)
 #remove cybersecurity
 myTechList.remove ("cybersecurity")
 
-#pop AWD
+#pop AWS
 myTechList.pop (5)
 
-#del HTML
+#del index 2
 del myTechList [2]
 
 print (myTechList)
